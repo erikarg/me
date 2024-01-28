fbc89e49eeeb8cd056ae3fa8d0fa538a2d8d979a
Erika Rocha <erika.rocha@zig.tickets>
2024-01-27 23:50:39 -0300

    checkpoint: fixes JSON and HTML structure

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function changeSlide(number) {
   const project = mainProjects[number];
   document.getElementById("project-title").innerText = project.nome;
   document.getElementById("project-objective").innerText = project.objetivo;
+
   const stacksList = document.getElementById("project-stacks");
   stacksList.innerHTML = Object.entries(project.stacks)
     .map(([key, value]) => `<li>${key}: ${value}</li>`)
@@ -21,7 +22,9 @@ function changeSlide(number) {
     .map(([key, route]) => `<div>${key}: ${route.metodo} - ${route.url}</div>`)
     .join("");
 
-  document.getElementById("project-link").href = project.link;
+  const projectLink = document.getElementById("project-link");
+  projectLink.href = project.link;
+  projectLink.style.color = "white";
 }
 
 items.forEach((item) => {
@@ -42,180 +45,145 @@ const mainProjects = [
     objetivo:
       "Construir uma loja de itens medievais no formato de uma API REST com CRUD.",
     stacks: {
-      backend: "Express, Node.js",
-      plataforma: "Docker",
-      arquitetura: "MSC",
-      linguagem: "Typescript",
-      banco: "MySQL",
+      Backend: "Express, Node.js",
+      Plataforma: "Docker",
+      Linguagem: "Typescript",
+      Banco: "MySQL",
     },
     rotas: {
-      login: {
+      Login: {
         metodo: "POST",
-        url: "http://localhost:3000/login",
+        url: "/login",
       },
-      users: {
+      Users: {
         metodo: "GET",
-        url: "http://localhost:3000/users",
+        url: "/users",
       },
-      products: {
+      Products: {
         metodo: "GET",
-        url: "http://localhost:3000/products",
+        url: "/products",
       },
-      orders: {
+      Orders: {
         metodo: "GET",
-        url: "http://localhost:3000/orders",
+        url: "/orders",
       },
-      link: "https://github.com/erikarg/Project-Trybesmith",
     },
+    link: "https://github.com/erikarg/Project-Trybesmith",
   },
   {
     nome: "Blogs API",
     objetivo:
       "Construir uma API Rest e um banco de dados para a produção de conteúdo para um blog.",
     stacks: {
-      backend: "Express, Node.js, Sequelize",
-      plataforma: "Docker",
-      linguagem: "JavaScript",
-      banco: "MySQL",
+      Backend: "Express, Node.js, Sequelize",
+      Plataforma: "Docker",
+      Linguagem: "JavaScript",
+      Banco: "MySQL",
     },
     rotas: {
-      login: {
+      Login: {
         metodo: "POST",
-        url: "http://localhost:3000/login",
+        url: "/login",
       },
-      users: {
+      Users: {
         metodo: "GET",
-        url: "http://localhost:3000/users",
+        url: "/users",
       },
-      blogPost: {
+      BlogPost: {
         metodo: "GET",
-        url: "http://localhost:3000/post",
+        url: "/post",
       },
-      categories: {
+      Categories: {
         metodo: "GET",
-        url: "http://localhost:3000/categories",
+        url: "/categories",
       },
-      link: "https://github.com/erikarg/Project-Blogs-API",
     },
+    link: "https://github.com/erikarg/Project-Blogs-API",
   },
   {
     nome: "Talker Manager",
     objetivo:
       "Construir uma aplicação de cadastro de palestrantes em que é possível cadastrar, visualizar, pesquisar, editar e excluir informações.",
     stacks: {
-      backend: "Express, Node.js",
-      plataforma: "Docker",
-      linguagem: "JavaScript",
+      Backend: "Express, Node.js",
+      Plataforma: "Docker",
+      Linguagem: "JavaScript",
     },
     rotas: {
-      login: {
+      Login: {
         metodo: "POST",
-        url: "http://localhost:3000/login",
+        url: "/login",
       },
-      talker: {
-        listar: {
-          metodo: "GET",
-          url: "http://localhost:3000/talker",
-        },
-        pesquisar: {
-          metodo: "GET",
-          url: "http://localhost:3000/talker/search",
-        },
-        visualizar: {
-          metodo: "GET",
-          url: "http://localhost:3000/talker/:id",
-        },
-        cadastrar: {
-          metodo: "POST",
-          url: "http://localhost:3000/talker",
-        },
-        editar: {
-          metodo: "PUT",
-          url: "http://localhost:3000/talker/:id",
-        },
-        excluir: {
-          metodo: "DELETE",
-          url: "http://localhost:3000/talker/:id",
-        },
+      Listar: {
+        metodo: "GET",
+        url: "/talker",
       },
-      link: "https://github.com/erikarg/Project-Talker-Manager",
+      Pesquisar: {
+        metodo: "GET",
+        url: "/talker/search",
+      },
+      Visualizar: {
+        metodo: "GET",
+        url: "/talker/:id",
+      },
+      Cadastrar: {
+        metodo: "POST",
+        url: "/talker",
+      },
+      Editar: {
+        metodo: "PUT",
+        url: "/talker/:id",
+      },
+      Excluir: {
+        metodo: "DELETE",
+        url: "/talker/:id",
+      },
     },
+    link: "https://github.com/erikarg/Project-Talker-Manager",
   },
   {
     nome: "Car Shop",
     objetivo:
       "Desenvolver uma API feita em TypeScript e MongoDB (utilizando Mongoose) para gerenciar o banco de dados de uma concessionária de veículos.",
     stacks: {
-      backend: "MongoDB, Mongoose",
-      plataforma: "Docker",
-      linguagem: "TypeScript",
+      Backend: "Mongoose",
+      Plataforma: "Docker",
+      Linguagem: "TypeScript",
+      Banco: "MongoDB",
     },
     rotas: {
-      carros: {
-        cadastrar: {
-          requisicao: "POST",
-          funcao: "Cadastra um carro",
-          url: "http://localhost:3001/cars",
-        },
-        listar: {
-          requisicao: "GET",
-          funcao: "Retorna a lista de carros",
-          url: "http://localhost:3001/cars",
-        },
-        visualizar: {
-          requisicao: "GET",
-          funcao: "Retorna o carro através do ID",
-          url: "http://localhost:3001/cars/:id",
-        },
-        atualizar: {
-          requisicao: "PUT",
-          funcao: "Atualiza o carro através do ID",
-          url: "http://localhost:3001/cars/:id",
-        },
-        excluir: {
-          requisicao: "DELETE",
-          funcao: "Exclui o carro através do ID",
-          url: "http://localhost:3001/cars/:id",
-        },
+      Cadastrar: {
+        metodo: "POST",
+        url: "/cars - /motorcycles",
       },
-      motocicletas: {
-        cadastrar: {
-          requisicao: "POST",
-          funcao: "Cadastra uma moto",
-          url: "http://localhost:3001/motorcycles",
-        },
-        listar: {
-          requisicao: "GET",
-          funcao: "Retorna a lista de motos",
-          url: "http://localhost:3001/motorcycles",
-        },
-        visualizar: {
-          requisicao: "GET",
-          funcao: "Retorna a moto através do ID",
-          url: "http://localhost:3001/motorcycles/:id",
-        },
-        atualizar: {
-          requisicao: "PUT",
-          funcao: "Atualiza a moto através do ID",
-          url: "http://localhost:3001/motorcycles/:id",
-        },
-        excluir: {
-          requisicao: "DELETE",
-          funcao: "Exclui a moto através do ID",
-          url: "http://localhost:3001/motorcycles/:id",
-        },
+      Listar: {
+        metodo: "GET",
+        url: "/cars - /motorcycles",
       },
-      link: "https://github.com/erikarg/Project-Car-Shop",
+      Visualizar: {
+        metodo: "GET",
+        url: "/cars/:id - /motorcycles/:id",
+      },
+      Atualizar: {
+        metodo: "PUT",
+
+        url: "/cars/:id - /motorcycles/:id",
+      },
+      Excluir: {
+        metodo: "DELETE",
+        url: "/cars/:id - /motorcycles/:id",
+      },
     },
+    link: "https://github.com/erikarg/Project-Car-Shop",
   },
   {
     nome: "Job Insights",
     objetivo:
       "Implementar análises a partir de um conjunto de dados sobre empregos (extraídos do Glassdoor e obtidos através do Kaggle). Essas análises são incorporadas a um aplicativo web desenvolvido com Flask. A aplicação também conta com testes realizados com Pytest.",
     stacks: {
-      linguagem: "Python",
-      framework: "Flask",
-      testes: "Pytest",
+      Linguagem: "Python",
+      Framework: "Flask",
+      Testes: "Pytest",
     },
     rotas: {},
     link: "https://github.com/erikarg/Project-Job-Insights",
